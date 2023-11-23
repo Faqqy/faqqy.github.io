945c13b4a056e2e5cf0f4c6b38bdc5449a69d812
Faqqy <dmitrybobkov5@gmail.com>
2023-11-23 23:19:58 +0300

    commit

--- js/index.js
+++ js/index.js
@@ -26,12 +26,6 @@ clickCards.forEach((items) => {
         }
     })
 })
-// clickCards.addEventListener('click', (event) => {
-//     if (event.target.classList.contains('text')) {
-//         const textElement = event.target.textContent
-//         openModal(textElement)
-//     }
-// })
 
 document.addEventListener('click',(event) => {
     const isModalOpened = event.target.classList.contains('opened')
@@ -42,40 +36,6 @@ document.addEventListener('click',(event) => {
         body.classList.remove('overflowActive')
     }
 })
-
-
-
-
-
-
-//
-//
-// function closePopups() {
-//     nextReadPopup.forEach(popup => {
-//         popup.classList.remove(activeClass);
-//     });
-// }
-//
-// document.addEventListener('click', function(event) {
-//     const isClickInsidePopup = Array.from(nextRead).some(popup => popup.contains(event.target));
-//     const isClickInsideTrigger = Array.from(nextReadPopup).some(trigger => trigger === event.target);
-//
-//     if (!isClickInsidePopup && !isClickInsideTrigger) {
-//         closePopups();
-//     }
-// });
-//
-// function handleClick() {
-//     nextReadPopup.forEach(function(element) {
-//         element.classList.remove(activeClass);
-//     });
-//     const index = Array.from(nextRead).indexOf(this);
-//     nextReadPopup[index].classList.add(activeClass);
-// }
-//
-// nextRead.forEach(function(element) {
-//     element.addEventListener('click', handleClick);
-// });
 
 
 
