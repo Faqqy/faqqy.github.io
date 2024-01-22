8c0eec804949044ad5aa542ffa8344c7fae11597
Faqqy <dmitrybobkov5@gmail.com>
2024-01-22 22:55:19 +0300

    commit

---
+++ 6f05173a6a2affb5d7a5.ts
@@ -0,0 +1,75 @@
+import '../libss/bootstrap/bootstrap-reboot.min.css';
+import '../libss/bootstrap/bootstrap-grid.min.css';
+import "./index.html";
+import "./blog-page.html";
+import './main.css';
+import Swiper from 'swiper';
+import 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+var swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 50,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
+document.addEventListener('mousemove', function (e) {
+    Object.assign(document.documentElement, {
+        style: "\n        --move-x: ".concat((e.clientX - window.innerWidth / 2) * -.005, "deg;\n        --move-y: ").concat((e.clientY - window.innerHeight / 2) * -.01, "deg;\n        ")
+    });
+});
+var observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+        var lamp = entry.target.querySelector('.idea__block--img');
+        var marquee = entry.target.querySelector('.marquee__block-text');
+        var marquee2 = entry.target.querySelector('.marquee__block-text2');
+        var tablets = entry.target.querySelector('.tablets__square');
+        if (entry.isIntersecting) {
+            lamp.classList.add('lamp-animation');
+            marquee.classList.add('marqueeAnim');
+            marquee2.classList.add('marqueeAnim');
+            tablets.classList.add('tabletsAnim');
+            return;
+        }
+        lamp.classList.remove('lamp-animation');
+        marquee.classList.remove('marqueeAnim');
+        marquee2.classList.remove('marqueeAnim');
+        tablets.classList.remove('tabletsAnim');
+    });
+});
+observer.observe(document.querySelector('.wrapper__container--idea'));
+var textElement = document.getElementById('myText');
+var textElement2 = document.getElementById('myText2');
+var blockScroll = document.getElementById('handle');
+var blockScroll2 = document.getElementById('handle2');
+var blockScroll3 = document.getElementById('drumsss');
+var leftImage = document.getElementById('leftImage');
+var rightImage = document.getElementById('rightImage');
+function handleScroll() {
+    var targetBlockPosition = blockScroll.offsetTop;
+    var targetBlockPosition2 = blockScroll2.offsetTop;
+    var targetBlockPosition3 = blockScroll3.offsetTop;
+    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    if (scrollPosition >= targetBlockPosition) {
+        textElement.style.opacity = String(1);
+    }
+    else {
+        textElement.style.opacity = String(0);
+    }
+    if (scrollPosition >= targetBlockPosition2) {
+        textElement2.style.opacity = String(1);
+        leftImage.style.left = String('60px');
+        rightImage.style.right = String('175px');
+    }
+    else {
+        leftImage.style.left = String();
+        rightImage.style.right = String();
+        textElement2.style.opacity = String(0);
+    }
+}
+window.addEventListener('scroll', handleScroll);
